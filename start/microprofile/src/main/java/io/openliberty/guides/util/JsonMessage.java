60afa7b8a09b97236faf670afa94dd8eb5d38506
yeekangc <yeekangc@ca.ibm.com>
2017-09-13 11:56:41 -0400

    Misc cleanup

--- start/microprofile/src/main/java/io/openliberty/guides/util/JsonMessage.java
+++ start/microprofile/src/main/java/io/openliberty/guides/util/JsonMessage.java
@@ -1,14 +1,26 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::comment[]
 package io.openliberty.guides.util;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 
 public enum JsonMessage {
-    
+
     SERVICE_UNREACHABLE();
-    
+
     private JsonObject json;
-    
+
     public JsonObject getJson() {
         switch(this) {
             case SERVICE_UNREACHABLE:
@@ -19,7 +31,7 @@ public enum JsonMessage {
         }
         return json;
     }
-    
+
     private void serviceUnreachable() {
         json = Json.createObjectBuilder()
                 .add("ERROR", "Unknown hostname or the resource may not be running on the host machine")
